08f032e1d4ac6e045ff751873b1e4f8bb83972d7
Charan <charan@Ubuntu.myguest.virtualbox.org>
2024-03-17 12:09:33 -0600

    fixed commit for aesd socket issue

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = d06883848264cd8a4d2e834ece04d74494061856
+AESD_ASSIGNMENTS_VERSION = 6f5b80c44127e4fe0baec1d2bc9f8f951ada5bc6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
