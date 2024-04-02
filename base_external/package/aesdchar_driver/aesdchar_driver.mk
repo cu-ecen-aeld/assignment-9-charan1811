fabc561de4dbd69e21320d8e0f3938a38d464f4e
Charan <charan@Ubuntu.myguest.virtualbox.org>
2024-04-01 21:15:25 -0600

    updated aesd socket hash

--- base_external/package/aesdchar_driver/aesdchar_driver.mk
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 8 git contents
-AESDCHAR_DRIVER_VERSION = '750ab5b1e8856170ed62cdc462092e4235438a02'
+AESDCHAR_DRIVER_VERSION = '234ffb68eafdddb50861d78f229b47ef4d475d6a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
